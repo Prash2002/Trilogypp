25115f4afa2bfa0f876e711ba4537aaaa275c99b
sonuishaq67 <sonuishaq67@gmail.com>
2020-07-21 20:21:04 +0530

    stless to stful and changed google login

--- sus_dev/lib/pages/Login.dart
+++ sus_dev/lib/pages/Login.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class Login extends StatelessWidget {
+final GoogleSignIn googleSignIn = GoogleSignIn();
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  login() {
+    googleSignIn.signIn();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    googleSignIn.signInSilently(suppressErrors: false).then((account) {
+      if (account != null) {
+        print('User Signed in $account');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -21,16 +43,9 @@ class Login extends StatelessWidget {
           SignInButton(
             Buttons.Google,
             onPressed: () {
-              Navigator.pushReplacementNamed(context, '/home');
+              onPressed:
+              () => login();
             },
-          ),
-          Divider(),
-          SignInButton(
-            Buttons.GoogleDark,
-            onPressed: () {
-              Navigator.pushReplacementNamed(context, '/home');
-            },
-            text: 'Sign up with google ',
           ),
           Divider(),
           SignInButton(
