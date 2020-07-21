76b15a5ddf7a593347fc6ae1be23838fb58e6f10
Prashu <1707prash@gmail.com>
2020-07-21 11:21:55 +0530

    Removed error in test

--- sus_dev/lib/main.dart
+++ sus_dev/lib/main.dart
@@ -3,11 +3,18 @@ import 'package:sus_dev/pages/Loading.dart';
 import 'package:sus_dev/pages/Home.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
     initialRoute: '/',
     routes: {
       '/': (context) => Loading(),
       '/home': (context) => Home(),
     },
-  ));
+    );
+  }
 }
