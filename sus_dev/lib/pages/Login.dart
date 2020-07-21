a3c49862aed595b8186b205e4ef2a344331d9359
sonuishaq67 <sonuishaq67@gmail.com>
2020-07-21 22:19:37 +0530

    added firebase auth only for google

--- sus_dev/lib/pages/Login.dart
+++ sus_dev/lib/pages/Login.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
+final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
+Future<String> signInWithGoogle() async {
+  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+  final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.getCredential(
+    accessToken: googleSignInAuthentication.accessToken,
+    idToken: googleSignInAuthentication.idToken,
+  );
+
+  final AuthResult authResult = await _auth.signInWithCredential(credential);
+  final FirebaseUser user = authResult.user;
+
+  assert(!user.isAnonymous);
+  assert(await user.getIdToken() != null);
+
+  final FirebaseUser currentUser = await _auth.currentUser();
+  assert(user.uid == currentUser.uid);
+
+  return 'signInWithGoogle succeeded: $user';
+}
+
+void signOutGoogle() async {
+  await googleSignIn.signOut();
+
+  print("User Sign Out");
+}
 
 class Login extends StatefulWidget {
   @override
@@ -43,8 +72,9 @@ class _LoginState extends State<Login> {
           SignInButton(
             Buttons.Google,
             onPressed: () {
-              onPressed:
-              () => login();
+              signInWithGoogle().whenComplete(() {
+                Navigator.pushReplacementNamed(context, '/home');
+              });
             },
           ),
           Divider(),
